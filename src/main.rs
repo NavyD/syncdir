bc9c1a4fe1df142c76e89add61f97285d1cae657
navyd <dhjnavyd@gmail.com>
2023-09-15 20:20:33 +0800

    feat: Add base sync command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{path::PathBuf, process::exit};
 
-use anyhow::{anyhow, bail, Result};
-use clap::{Parser, Subcommand};
+use anyhow::{bail, Result};
+use clap::{value_parser, Parser, Subcommand};
 use directories::ProjectDirs;
 use itertools::Itertools;
 use log::LevelFilter;
@@ -21,11 +21,38 @@ fn main() {
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// log level
-    #[arg(short, long, default_value_t = 3, action = clap::ArgAction::Count)]
+    #[arg(short, long, default_value_t = 0, action = clap::ArgAction::Count, value_parser = value_parser!(u8).range(0..=5))]
     verbose: u8,
+
+    #[arg(short, long, default_value = ProjectDirs::from("xyz", "navyd", env!("CARGO_CRATE_NAME"))
+        .map(|p| p.config_dir().display().to_string())
+        .unwrap_or_default())]
+    config_dir: PathBuf,
 
     #[command(subcommand)]
     command: Commands,
+}
+
+impl Args {
+    fn validate(&self) -> Result<()> {
+        let mut paths = vec![&self.config_dir];
+        match &self.command {
+            Commands::Apply { sub_args } | Commands::Sync { sub_args } => {
+                paths.push(&sub_args.src);
+                paths.push(&sub_args.dst);
+                if let Some(t) = &sub_args.target_dsts {
+                    paths.extend(t)
+                }
+            }
+            _ => {}
+        }
+        for p in paths {
+            if !p.exists() {
+                bail!("Not found path {}", p.display());
+            }
+        }
+        Ok(())
+    }
 }
 
 #[derive(clap::Args, Debug, Clone)]
@@ -45,12 +72,13 @@ struct SubArgs {
 
 #[derive(Debug, Subcommand, Clone)]
 enum Commands {
-    /// Adds files to myapp
+    /// 将src文件应用到dst中
     Apply {
         #[command(flatten)]
         sub_args: SubArgs,
     },
 
+    /// 将dst的文件同步到src中
     Sync {
         #[command(flatten)]
         sub_args: SubArgs,
@@ -61,20 +89,18 @@ enum Commands {
 
 #[derive(Debug, Clone)]
 struct Cli {
-    pdir: ProjectDirs,
     args: Args,
 }
 
 impl Cli {
     fn new() -> Result<Self> {
         Ok(Self {
-            pdir: ProjectDirs::from("xyz", "navyd", env!("CARGO_CRATE_NAME"))
-                .ok_or_else(|| anyhow!("Failed to get project dirs"))?,
             args: Args::parse(),
         })
     }
 
     fn run(&self) -> Result<()> {
+        self.args.validate()?;
         self.set_log()?;
 
         match &self.args.command {
@@ -82,13 +108,32 @@ impl Cli {
                 let sync = self.build_syncer(sub_args)?;
                 apply(&sync, sub_args)?;
             }
+            Commands::Sync { sub_args } => {
+                let sync = self.build_syncer(sub_args)?;
+                println!(
+                    "{}Syncing back from dst {} to src {}",
+                    if sub_args.dry_run { "Dry running " } else { "" },
+                    sub_args.dst.display(),
+                    sub_args.src.display(),
+                );
+                let paths = sync.sync_back(sub_args.target_dsts.as_deref().unwrap_or_default())?;
+                println!(
+                    "{}Sync back total {} paths:",
+                    if sub_args.dry_run { "Dry running " } else { "" },
+                    paths.len()
+                );
+                paths
+                    .iter()
+                    .sorted_unstable()
+                    .for_each(|p| println!("{:?}", p));
+            }
             _ => todo!(),
         }
         Ok(())
     }
 
     fn build_syncer(&self, sub_args: &SubArgs) -> Result<Syncer> {
-        let last_dsts_p = self.pdir.config_dir().join("last-dsts");
+        let last_dsts_p = self.args.config_dir.join("last-dsts");
         let last_dsts_srv = LastDestinationListServiceBuilder::default()
             .path(last_dsts_p)
             .build()?;
